5fc9da3555078c2e1eb1ac24e01d8a6f883f1951
Peter <schmidt.multimedia@gmail.com>
2022-11-28 11:19:49 +0100

    variante mit while ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,22 @@ public class App {
     
         Player player = new Player();
 
-        for (int i = 0; i < 12; i++) {
-            ausgabe(player.play());  
+        
+        // Variante 1
+        // for (int i = 0; i < 12; i++) {
+        //     output(player.play());  
+        // }
+
+        // Variante 2
+        while (player.points < 13) {
+            output(player.play()); 
         }
 
+
+       
     }
 
-    private static void ausgabe(String outStr) {
+    private static void output(String outStr) {
         System.out.println(outStr);
     }
 }
